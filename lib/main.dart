527d1812f94d4825fb697d9bc36d4edd11620530
bymadmr@outlook.com <bymadmr@outlook.com>
2021-04-22 01:36:02 +0300

    Making the box come with animation

--- lib/main.dart
+++ lib/main.dart
@@ -22,18 +22,47 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int tabsCount = 0;
   int doubleTabsCount = 0;
   int longPressCount = 0;
   double xPosition = 0;
   double yPosition = 0;
-  double boxsize = 150;
+  double boxSize = 0;
+  double fullBoxSize = 150;
+
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    //when the application is first opened
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 1000), //How long will it take?
+        vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeInOut);
+    animation.addListener(() {
+      setState(() {
+        boxSize = fullBoxSize * animation.value;
+      });
+      centerBox(context);
+    });
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (xPosition == 0) {
-      centerBox(context);
-    }
+    // if (xPosition == 0) {
+    //   centerBox(context);
+    // }
     return Scaffold(
       appBar: AppBar(
         title: Text("Gestures Project"),
@@ -72,8 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 left: xPosition,
                 top: yPosition,
                 child: Container(
-                  width: boxsize,
-                  height: boxsize,
+                  width: boxSize,
+                  height: boxSize,
                   decoration: BoxDecoration(color: Colors.cyan),
                 ))
           ],
@@ -89,8 +118,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void centerBox(BuildContext context) {
-    xPosition = MediaQuery.of(context).size.width / 2 - boxsize / 2;
-    yPosition = MediaQuery.of(context).size.height / 2 - boxsize / 2 - 30;
+    xPosition = MediaQuery.of(context).size.width / 2 - boxSize / 2;
+    yPosition = MediaQuery.of(context).size.height / 2 - boxSize / 2 - 30;
     setState(() {
       this.xPosition = xPosition;
       this.yPosition = yPosition;
